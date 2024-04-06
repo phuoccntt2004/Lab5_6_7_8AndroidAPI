8afffc906e0376063924b608f4189b00e5366207
phuocdz5 <phuoccntt2004@gmail.com>
2024-04-06 16:44:29 +0700

    done lab 5 6 7

---
+++ app/src/main/java/com/example/Lab_AndroidAPI/services/ApiServices.java
@@ -0,0 +1,94 @@
+package com.example.Lab_AndroidAPI.services;
+
+import com.example.Lab_AndroidAPI.model.Distributor;
+import com.example.Lab_AndroidAPI.model.Fruit;
+import com.example.Lab_AndroidAPI.model.Page;
+import com.example.Lab_AndroidAPI.model.Response;
+import com.example.Lab_AndroidAPI.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.PartMap;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+
+public interface ApiServices {
+
+    public static String IP = "172.16.49.197";
+    public static String BASE_URL = "http://"+IP+":3000/api/";
+
+    @GET("get-list-distributor")
+    Call<Response<ArrayList<Distributor>>> getListDistributor();
+
+    @GET("search-distributors")
+    Call<Response<ArrayList<Distributor>>> getSearchDistributors(@Query("key") String key);
+
+    @DELETE("delete-distributors/{id}")
+    Call<Response<Distributor>> getDeleteDistributor(@Path("id") String id);
+
+    @POST("add-distributor")
+    Call<Response<Distributor>> addDistributor(@Body Distributor distributor);
+    @PUT("update-distributors/{id}")
+    Call<Response<Distributor>> getUpdateDistributor(@Path("id") String id, @Body Distributor distributor);
+
+    //lab 6
+    @Multipart
+    @POST("register-send-email")
+    Call<Response<User>> register(
+            @Part("username") RequestBody username,
+            @Part("password") RequestBody password,
+            @Part("email") RequestBody email,
+            @Part("name") RequestBody name,
+            @Part MultipartBody.Part avartar
+
+    );
+
+    @POST("login")
+    Call<Response<User>> login (@Body User user);
+
+    @GET("get-list-fruit")
+    Call<Response<ArrayList<Fruit>>> getListFruit(@Header("Authorization")String token);
+
+    @Multipart
+    @POST("add-fruit-with-file-image")
+    Call<Response<Fruit>> addFruitWithFileImage(@PartMap Map<String, RequestBody> requestBodyMap,
+                                                @Part ArrayList<MultipartBody.Part> ds_hinh
+    );
+
+
+    @GET("get-page-fruit")
+    Call<Response<Page<ArrayList<Fruit>>>> getPageFruit( @QueryMap Map<String, String> stringMap);
+
+
+    @Multipart
+    @PUT("update-fruit-by-id/{id}")
+    Call<Response<Fruit>> updateFruitWithFileImage(@PartMap Map<String, RequestBody> requestBodyMap,
+                                                   @Path("id") String id,
+                                                   @Part ArrayList<MultipartBody.Part> ds_hinh
+    );
+
+    @DELETE("destroy-fruit-by-id/{id}")
+    Call<Response<Fruit>> deleteFruits(@Path("id") String id);
+
+    @GET("get-fruit-by-id/{id}")
+    Call<Response<Fruit>> getFruitById (@Path("id") String id);
+
+
+
+
+}
+
